742331e5c44395854e042fea1f79817b4f3b7291
aniko33 <76649588+aniko33@users.noreply.github.com>
2023-09-02 15:55:15 +0200

    Update example_string.rs

--- src/example_string.rs
+++ src/example_string.rs
@@ -1,6 +1,8 @@
+mod rave;
+
 fn main() {
     let adr = Alloc::string("Hello").expect("Panic"); // Alloc String
-    let converted = rave::StrOut::decode(rave::Read::string(webhook).as_str()).expect("This is not a string") // Read & convert to String  
+    let converted = rave::StrOut::decode(rave::Read::string(adr)).expect("This is not a string"); // Read & convert to String  
     println!("{}", converted);
     Dealloc::string(adr); // Delete from memory
 }
